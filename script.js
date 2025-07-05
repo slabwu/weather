4c46922d6c105eb3e419ab614571a867949e5203
slabwu <134193743+slabwu@users.noreply.github.com>
2025-07-05 20:10:46 +0800

    Process JSON data

--- script.js
+++ script.js
@@ -4,9 +4,30 @@ async function fetchData(city) {
         let response = await fetch(url, {mode: 'cors'})
         let data = await response.json()
         console.log(data)
+
+        return processData(data)
     } catch (err) {
         console.log(new Error('Unable to fetch data'));
     }
 }
 
-fetchData('New York')
+function processData(data) {
+    let processed = {
+        address: data.address
+    }
+
+    let today = ['datetime', 'temp', 'conditions', 'feelslike', 'humidity', 'precipprob', 'uvindex', 'visibility']
+    today.forEach(value => processed[value] = data.currentConditions[value])
+
+    
+    let week = ['datetime', 'conditions', 'tempmax', 'tempmin']
+    processed.forecast = {}
+    for (i = 0; i < 5; i++) {
+        processed.forecast[i] = {}
+        week.forEach(value => processed.forecast[i][value] = data.days[i][value])
+    }
+
+    return processed
+}
+
+fetchData('New York').then(console.log)
